8aef918726876dd72d7f9a92bdf7b369644912a2
v9493394558s <45127814+v9493394558s@users.noreply.github.com>
2021-01-17 22:46:05 +0530

    updated 17-01-2021

--- db/conn.php
+++ db/conn.php
@@ -2,7 +2,9 @@
     require_once 'headers.php';
 
     // $connect = new PDO("mysql:host=localhost;dbname=vcrud", "root", "");
+   // you can change remote server address above
 
+   
  $connect = new PDO('sqlite:../db/vcrud.sqlite');
  $connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
